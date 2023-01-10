067da77d19bc8bd8bbbf48d204ba8a49919e4ccc
mahesh yadav <mahesh.yadav@clouddrove.com>
2023-01-10 17:13:19 +0530

    inital commit

--- example/mysql-flexible-server/output.tf
+++ example/mysql-flexible-server/output.tf
@@ -1,18 +1,18 @@
-output "mysql_flexible_server_id" {
-  value       = module.mysql_flexible.mysql_flexible_server_id
+output "flexible-mysql_server_id" {
+  value       = module.flexible-mysql.mysql_flexible_server_id
   description = "The ID of the MySQL Flexible Server."
 }
 output "azurerm_private_dns_zone_virtual_network_link_id" {
-  value       = module.mysql_flexible.azurerm_private_dns_zone_virtual_network_link_id
+  value       = module.flexible-mysql.azurerm_private_dns_zone_virtual_network_link_id
   description = "The ID of the Private DNS Zone Virtual Network Link."
 }
-output "azurerm_mysql_flexible_server_configuration_id" {
-  value       = module.mysql_flexible.azurerm_mysql_flexible_server_configuration_id
+output "azurerm_flexible-mysql_server_configuration_id" {
+  value       = module.flexible-mysql.azurerm_mysql_flexible_server_configuration_id
   description = "The ID of the MySQL Flexible Server Configuration."
 }
 
 output "azurerm_private_dns_zone_id" {
-  value       = module.mysql_flexible.azurerm_private_dns_zone_id
+  value       = module.flexible-mysql.azurerm_private_dns_zone_id
   description = "The Private DNS Zone ID."
 }
 
