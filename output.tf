e0cd9c5df502504bccec5f7e83ba493a9b0481f5
Vishal Sharma <vishal.sharma@clouddrove.com>
2024-09-12 01:53:06 +0530

    fix:fixed tflint warnings

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "mysql_flexible_server_id" {
-  value       = join("", azurerm_mysql_flexible_server.main.*.id)
+  value       = join("", azurerm_mysql_flexible_server.main.[0].id)
   description = "The ID of the MySQL Flexible Server."
 }
 
 output "azurerm_private_dns_zone_virtual_network_link_id" {
-  value       = join("", azurerm_private_dns_zone_virtual_network_link.main.*.id)
+  value       = join("", azurerm_private_dns_zone_virtual_network_link.main.[0].id)
   description = "The ID of the Private DNS Zone Virtual Network Link."
 }
 output "existing_private_dns_zone_virtual_network_link_id" {
-  value       = join("", azurerm_private_dns_zone_virtual_network_link.main2.*.id)
+  value       = join("", azurerm_private_dns_zone_virtual_network_link.main2.[0].id)
   description = "The ID of the Private DNS Zone Virtual Network Link."
 }
 output "azurerm_mysql_flexible_server_configuration_id" {
