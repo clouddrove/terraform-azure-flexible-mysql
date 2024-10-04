b332a11bc5471de217fe0877e3d33fa05c5b9ab0
Vishal Sharma <vishal.sharma@clouddrove.com>
2024-10-04 17:00:34 +0530

    Fix:fixed variables

--- main.tf
+++ main.tf
@@ -208,7 +208,7 @@ resource "azurerm_key_vault_key" "primary_cmk_key" {
 
 resource "azurerm_role_assignment" "primary_cmk_role_assignment" {
   count                = var.key_vault_with_rbac && var.cmk_enabled ? 1 : 0
-  principal_id         = azurerm_user_assigned_identity.primary_cmk[0].principal_id
+  principal_id         = azurerm_user_assigned_identity.primary_cmk_umi[0].principal_id
   role_definition_name = "Key Vault Crypto Service Encryption User"
   scope                = var.key_vault_id
 }
@@ -217,7 +217,7 @@ resource "azurerm_key_vault_access_policy" "primary_cmk_access_policy" {
   count           = !var.key_vault_with_rbac && var.cmk_enabled ? 1 : 0
   key_vault_id    = var.key_vault_id
   tenant_id       = data.azurerm_client_config.current.tenant_id
-  object_id       = azurerm_user_assigned_identity.primary_cmk[0].principal_id
+  object_id       = azurerm_user_assigned_identity.primary_cmk_umi[0].principal_id
   key_permissions = ["Get", "WrapKey", "UnwrapKey", "List"]
 }
 
@@ -239,7 +239,7 @@ resource "azurerm_key_vault_key" "geo_cmk_key" {
 
 resource "azurerm_role_assignment" "geo_cmk_role_assignment" {
   count                = var.key_vault_with_rbac && var.cmk_enabled && var.geo_redundant_backup_enabled ? 1 : 0
-  principal_id         = azurerm_user_assigned_identity.geo_cmk[0].principal_id
+  principal_id         = azurerm_user_assigned_identity.geo_cmk_umi[0].principal_id
   role_definition_name = "Key Vault Crypto Service Encryption User"
   scope                = var.key_vault_id
 }
@@ -248,6 +248,6 @@ resource "azurerm_key_vault_access_policy" "geo_cmk_access_policy" {
   count           = !var.key_vault_with_rbac && var.cmk_enabled && var.geo_redundant_backup_enabled ? 1 : 0
   key_vault_id    = var.key_vault_id
   tenant_id       = data.azurerm_client_config.current.tenant_id
-  object_id       = azurerm_user_assigned_identity.geo_cmk[0].principal_id
+  object_id       = azurerm_user_assigned_identity.geo_cmk_umi[0].principal_id
   key_permissions = ["Get", "WrapKey", "UnwrapKey", "List"]
 }
